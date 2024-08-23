5d0cafc0f568a148781cba11336d1fc2c26715f1
Infinite-Learner <152425498+Infinite-Learner@users.noreply.github.com>
2024-08-23 15:09:55 +0530

    Update Real_Db_Constructor.py

--- Python/Real_Db_Constructor.py
+++ Python/Real_Db_Constructor.py
@@ -1,70 +1,87 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # coding: utf-8
+from dataclasses import field
 
 # In[13]:
 
 
-#import mysql.connector
-'''def con(host_in="",user_in="",password_in=""):
-    mycon = mysql.connector.connect(host=host_in,user=user_in,password=password_in)
-    print("Connected Successfully")
-    cur = mycon.cursor()
-    return cur'''
-import sqlite3
-def table():
+import mysql.connector
+
+cur = ""
+
+
+def connection(host_in="", user_in="", password_in=""):
+    cnx={
+       "host":host_in,
+        "user":user_in,
+        "password":password_in
+    }
+    con = mysql.connector.connect(**cnx)
+    return con
+
+def create(cur):
+    qr = f"CREATE TABLE"
+    fields_count = int(input("Enter No of Fields : "))
+    Field_Attributes={"Data_type":"",
+                                    "Length":"",
+                                    "PRIMARY":False,
+                                    "UNIQUE":False,
+                                    "FOREIGN":False,
+                                    "Auto-Increment":False,
+                                    "NOT NULL":False,
+                                   }
+    for i in range(0,fields_count):
+        field_name = input("Field_Name : ")
+        for j in Field_Attributes:
+            Field_Attributes[j] = input(f"Enter {field_name}-->{j} : ")
+        print(*Field_Attributes)
+def table(cur):
     inp = int(input('''***Table Manupulation***
                     1.CREATE
                     2.INSERT
                     3.DELETE
-                    4.UPDATE'''))
+                    4.UPDATE
+                    Enter need : '''))
     if(inp == 1):
-        create();
-        
-        
-def create():
-	stmt = ""
-	T_Name = input("Enter Table Name : ")
-	T_Col = int(input("Enter No of Columns : "))
-	qrp=['Name','Data_Type',';.']
-	for i in range(0,T_Col):
-		qry = input("Enter Field "+i+"Name : ")
-'''def init(ch):
-    if(ch==1):
+       create(cur);
+
+
+def init(ch, cur):
+    if (ch == 1):
+        cur.execute("show databases")
+        print(cur.fetchall())
         stmt = input("Enter DB_Name : ")
-        cur.execute(f"USE DATABASE {stmt}")
-        table();
-    elif(ch==2):
+        cur.execute(f"USE {stmt}")
+        table(cur);
+    elif (ch == 2):
         stmt = input("Enter DB_Name : ")
         cur.execute(f"CREATE DATABASE {stmt}")
         cur.execute(f"USE {stmt}")
-        table();
-def main():
-    ch = int(input(''''''***Select your choice***
+        table(cur);
+
+
+def main(cur):
+    ch = int(input('''***Select your choice***
     1.Existing Database
     2.New DataBase
-    Enter Your Choice : 
-    init(ch);'''
+    Enter Your Choice : '''))
+    init(ch, cur);
+
+
 print("Welcome to DB_Controller")
-cur=sqlite3.connect("DemoDb.db").cursor()
-table()
 host_in = input("Enter Host name : ")
-#port = int(input("Enter Port Number : "))
+# port = int(input("Enter Port Number : "))
 user_in = input("Enter username : ")
 password_in = input("Password : ")
-#cur = con(host_in,user_in,password_in)
-main()
+cur = connection(host_in, user_in, password_in)
+main(cur.cursor())
+
+# In[ ]:
 
 
 # In[ ]:
 
-
-#main()
-
-
-# In[ ]:
-'''
-
-ddl = ['CREATE','ALTER','DROP']
-dml = ['INSERT','DELETE','UPDATE','SELECT']
-con = ['WHERE']
-'''
+#
+# ddl = ['CREATE','ALTER','DROP']
+# dml = ['INSERT','DELETE','UPDATE','SELECT']
+# con = ['WHERE']
